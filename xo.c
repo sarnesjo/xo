9ef6b0d7e61a2710c7b9c911b1be29f88c640f7a
Jesper Särnesjö <jesper@sarnesjo.org>
2011-03-29 16:33:51 +0200

    Added "generate graph" action.

--- xo.c
+++ xo.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "graph.h"
 #include "insns.h"
 #include "xo.h"
 
 typedef enum
 {
+  XO_ACTION_GENERATE_GRAPH,
   XO_ACTION_LIST_INSNS,
   XO_ACTION_SHOW_HELP,
   XO_ACTION_SHOW_VERSION,
 } xo_action;
 
-xo_action action = XO_ACTION_SHOW_HELP;
+xo_action action = XO_ACTION_GENERATE_GRAPH;
 int verbosity;
+int num_inputs, num_insns;
+
+void graph_callback(const xo_graph *graph)
+{
+  xo_graph_print(graph);
+}
+
+void generate_graph()
+{
+  xo_graph *graph = xo_graph_create(num_inputs+num_insns);
+  xo_supergraph *supergraph = xo_supergraph_create(num_inputs+num_insns, num_inputs);
+  xo_graph_generate_from_supergraph(graph, supergraph, graph_callback);
+}
 
 void list_insns()
 {
@@ -27,7 +42,7 @@ void list_insns()
 void show_help()
 {
   printf("usage:\n");
-  printf("\t%s [-qv]\n", PACKAGE_NAME);
+  printf("\t%s [-qv] -i NUM_INPUTS -n NUM_INSNS\n", PACKAGE_NAME);
   printf("\t%s -L\n", PACKAGE_NAME);
   printf("\t%s -H\n", PACKAGE_NAME);
   printf("\t%s -V\n", PACKAGE_NAME);
@@ -41,7 +56,7 @@ void show_version()
 int main(int argc, char *argv[])
 {
   int o;
-  while((o = getopt(argc, argv, "LHVqv")) != -1)
+  while((o = getopt(argc, argv, "LHVqvi:n:")) != -1)
   {
     switch(o)
     {
@@ -60,6 +75,12 @@ int main(int argc, char *argv[])
       case 'v':
         ++verbosity;
         break;
+      case 'i':
+        num_inputs = strtol(optarg, NULL, 0);
+        break;
+      case 'n':
+        num_insns = strtol(optarg, NULL, 0);
+        break;
       default:
         exit(1);
     }
@@ -67,8 +88,14 @@ int main(int argc, char *argv[])
   argc -= optind;
   argv += optind;
 
+  if(action == XO_ACTION_GENERATE_GRAPH && !(num_inputs > 0 && num_insns > 0))
+    action = XO_ACTION_SHOW_HELP;
+
   switch(action)
   {
+    case XO_ACTION_GENERATE_GRAPH:
+      generate_graph();
+      break;
     case XO_ACTION_LIST_INSNS:
       list_insns();
       break;
