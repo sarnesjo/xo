4a0cb1d3eb0573bdcc34abd966ed4f3a6c5de2d1
Jesper Särnesjö <jesper@sarnesjo.org>
2011-05-18 23:15:03 +0200

    Added src/parser.{c,h} with stub for xo_parser_validate.

---
+++ src/parser.c
@@ -0,0 +1,6 @@
+#include "parser.h"
+
+bool xo_parser_validate(const char *input)
+{
+  return false;
+}
---
+++ src/parser.h
@@ -0,0 +1,8 @@
+#ifndef XO_PARSER_H
+#define XO_PARSER_H
+
+#include <stdbool.h>
+
+bool xo_parser_validate(const char *);
+
+#endif
