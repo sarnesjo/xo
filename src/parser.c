8c6af6183cb7fbac7e71b7894b0affb566f82500
Jesper Särnesjö <jesper@sarnesjo.org>
2011-05-18 23:24:37 +0200

    Implemented asm_input test and updated xo_parser_validate function to match.

--- src/parser.c
+++ src/parser.c
@@ -1,6 +1,47 @@
+#include <stdio.h>
 #include "parser.h"
+
+static bool insn_(const char *input, const char **remaining_input)
+{
+  char buf[8];
+  int num_chars_consumed = 0;
+
+  if(sscanf(input, " %7[a-z]%n", buf, &num_chars_consumed) != 1)
+    return false;
+  input += num_chars_consumed;
+  if(sscanf(input, " %7[a-z]%n", buf, &num_chars_consumed) != 1)
+    return false;
+  input += num_chars_consumed;
+  if(sscanf(input, " %1[,]%n", buf, &num_chars_consumed) != 1)
+    return false;
+  input += num_chars_consumed;
+  if(sscanf(input, " %7[a-z]%n", buf, &num_chars_consumed) != 1)
+    return false;
+  input += num_chars_consumed;
+  if(sscanf(input, " %1[;]%n", buf, &num_chars_consumed) != 1)
+    return false;
+  input += num_chars_consumed;
+
+  if(remaining_input)
+    *remaining_input = input;
+  return true;
+}
 
 bool xo_parser_validate(const char *input)
 {
-  return false;
+  if(!input)
+    return false;
+
+  // read all insns
+  while(insn_(input, &input));
+
+  // make sure nothing but whitespace remains (ugly)
+  while(*input)
+  {
+    if(*input != ' ')
+      return false;
+    ++input;
+  }
+
+  return true;
 }
--- tests/asm_input.c
+++ tests/asm_input.c
@@ -1,6 +1,81 @@
 #include "check.h"
+#include "../src/parser.h"
 
 int main()
 {
+  // basic invalid input
+  CHECK(!xo_parser_validate(NULL));
+
+  // basic valid input
+  CHECK(xo_parser_validate(""));
+  CHECK(xo_parser_validate("add a,b;"));
+
+  // require comma
+  CHECK(!xo_parser_validate("add a b;"));
+
+  // require semicolon
+  CHECK(!xo_parser_validate("add a,b"));
+
+  // require args
+  CHECK(!xo_parser_validate("add a;"));
+  CHECK(!xo_parser_validate("add;"));
+
+  // ignore whitespace
+  CHECK(xo_parser_validate(" add a , b ; "));
+  CHECK(xo_parser_validate(" add a , b ;"));
+  CHECK(xo_parser_validate(" add a , b; "));
+  CHECK(xo_parser_validate(" add a , b;"));
+  CHECK(xo_parser_validate(" add a ,b ; "));
+  CHECK(xo_parser_validate(" add a ,b ;"));
+  CHECK(xo_parser_validate(" add a ,b; "));
+  CHECK(xo_parser_validate(" add a ,b;"));
+  CHECK(xo_parser_validate(" add a, b ; "));
+  CHECK(xo_parser_validate(" add a, b ;"));
+  CHECK(xo_parser_validate(" add a, b; "));
+  CHECK(xo_parser_validate(" add a, b;"));
+  CHECK(xo_parser_validate(" add a,b ; "));
+  CHECK(xo_parser_validate(" add a,b ;"));
+  CHECK(xo_parser_validate(" add a,b; "));
+  CHECK(xo_parser_validate(" add a,b;"));
+  CHECK(xo_parser_validate("add a , b ; "));
+  CHECK(xo_parser_validate("add a , b ;"));
+  CHECK(xo_parser_validate("add a , b; "));
+  CHECK(xo_parser_validate("add a , b;"));
+  CHECK(xo_parser_validate("add a ,b ; "));
+  CHECK(xo_parser_validate("add a ,b ;"));
+  CHECK(xo_parser_validate("add a ,b; "));
+  CHECK(xo_parser_validate("add a ,b;"));
+  CHECK(xo_parser_validate("add a, b ; "));
+  CHECK(xo_parser_validate("add a, b ;"));
+  CHECK(xo_parser_validate("add a, b; "));
+  CHECK(xo_parser_validate("add a, b;"));
+  CHECK(xo_parser_validate("add a,b ; "));
+  CHECK(xo_parser_validate("add a,b ;"));
+  CHECK(xo_parser_validate("add a,b; "));
+  CHECK(xo_parser_validate("add a,b;"));
+
+  // multiple insns
+  CHECK(xo_parser_validate("add a,b; add a,b;"));
+
+  // ignore whitespace
+  CHECK(xo_parser_validate(" add a,b; add a,b; "));
+  CHECK(xo_parser_validate(" add a,b; add a,b;"));
+  CHECK(xo_parser_validate(" add a,b;add a,b; "));
+  CHECK(xo_parser_validate(" add a,b;add a,b;"));
+  CHECK(xo_parser_validate("add a,b; add a,b; "));
+  CHECK(xo_parser_validate("add a,b; add a,b;"));
+  CHECK(xo_parser_validate("add a,b;add a,b; "));
+  CHECK(xo_parser_validate("add a,b;add a,b;"));
+
+  // require semicolons
+  CHECK(!xo_parser_validate("add a,b add a,b"));
+  CHECK(!xo_parser_validate("add a,b add a,b;"));
+  CHECK(!xo_parser_validate("add a,b; add a,b"));
+
+  // catch trailing junk
+  CHECK(!xo_parser_validate("add a,b; a"));
+  CHECK(!xo_parser_validate("add a,b; ;"));
+  CHECK(!xo_parser_validate("add a,b; ?"));
+
   return EXIT_SUCCESS;
 }
