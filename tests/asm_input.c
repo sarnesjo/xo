3cbd0666f8376547cff624efe6ae232664f1c682
Jesper Särnesjö <jesper@sarnesjo.org>
2011-05-18 15:38:04 +0200

    Replaced check_xo test with asm_input.

---
+++ tests/asm_input.c
@@ -0,0 +1,6 @@
+#include "check.h"
+
+int main()
+{
+  return EXIT_SUCCESS;
+}
--- tests/check_xo.c
+++
@@ -1,7 +0,0 @@
-#include "check.h"
-
-int main(int argc, char *argv[])
-{
-  CHECK(2 + 2 == 5);
-  return EXIT_SUCCESS;
-}
