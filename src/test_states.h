3834b54e075c37c411db456dc4ad1131b265b599
Jesper Särnesjö <jesper@sarnesjo.org>
2011-05-26 15:26:10 +0200

    Added test_states.{c,h}.

---
+++ src/test_states.h
@@ -0,0 +1,11 @@
+#ifndef XO_TEST_STATES_H
+#define XO_TEST_STATES_H
+
+#include "types.h"
+
+#define XO_NUM_TEST_STATES 14
+
+extern const xo_machine_state xo_test_states[XO_NUM_TEST_STATES];
+
+#endif
+
