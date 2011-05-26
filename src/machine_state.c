f32bae3d7c4f52de0480a5f7404a86feb8216481
Jesper Särnesjö <jesper@sarnesjo.org>
2011-05-26 15:08:53 +0200

    Added xo_machine_state_{init,copy} functions.

--- src/machine_state.c
+++ src/machine_state.c
@@ -1,6 +1,20 @@
 #include "flag_set.h"
 #include "machine_state.h"
 
+void xo_machine_state_init(xo_machine_state *st, uint32_t reg_value, xo_flag_set set_flags)
+{
+  for(size_t i = 0; i < XO_NUM_REGISTERS; ++i)
+    st->regs[i] = reg_value;
+  st->flags = set_flags;
+}
+
+void xo_machine_state_copy(xo_machine_state *st1, const xo_machine_state *st2)
+{
+  for(size_t i = 0; i < XO_NUM_REGISTERS; ++i)
+    st1->regs[i] = st2->regs[i];
+  st1->flags = st2->flags;
+}
+
 void xo_machine_state_print(FILE *file, const xo_machine_state *st, const char *suffix)
 {
   for(size_t i = 0; i < XO_NUM_REGISTERS; ++i)
