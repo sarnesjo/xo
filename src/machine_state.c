a99ee10bc22a269f73e84df55fe24ecdf52079b4
Jesper Särnesjö <jesper@sarnesjo.org>
2011-05-26 15:08:26 +0200

    Removed cf, of, pf, sf, zf fields from xo_machine_state, and added flags field.

--- src/machine_state.c
+++ src/machine_state.c
@@ -1,8 +1,9 @@
+#include "flag_set.h"
 #include "machine_state.h"
 
 void xo_machine_state_print(FILE *file, const xo_machine_state *st, const char *suffix)
 {
-  for(int i = 0; i < XO_NUM_REGISTERS; ++i)
+  for(size_t i = 0; i < XO_NUM_REGISTERS; ++i)
     fprintf(file, "0x%08x ", st->regs[i]);
-  fprintf(file, "%d %d %d %d %d%s", st->cf, st->of, st->pf, st->sf, st->zf, suffix);
+  xo_flag_set_print(file, st->flags, suffix);
 }
